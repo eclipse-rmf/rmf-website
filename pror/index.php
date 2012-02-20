796e92ca616a9bfb7ebe983db8e45f1005db11a1
mjastram <mjastram>
2012-02-20 20:18:59 +0000

    ProR Subpage

--- pror/index.php
+++ pror/index.php
@@ -11,7 +11,7 @@
  *******************************************************************************/
 
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");
-	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");
+#	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");
 	
 	$App 	= new App();	
