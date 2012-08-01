e209a366d8c337ec26983f17ceb5814b8c40db8e
lladenberge <lladenberge>
2012-08-01 11:53:08 +0000

    test

--- download.php
+++ download.php
@@ -172,10 +172,10 @@
 	
 	$folder = $App->getDownloadBasePath()."/rmf/downloads/drops";
 
-	$html .= "<h4>Software Repository</h4>";
-	$html .= "<p>This project maintains a p2 repository of binary artifacts. Copy and paste this link into the "Install New Software" dialog to install this project's software.</p>";
-	$html .= "<i>http://download.eclipse.org/rmf/updates</i>";
-	$html .= "<p>Note that the repository link will not necessarily display anything meaningful in your browser.</p>";
+	//$html .= "<h4>Software Repository</h4>\n";
+	//$html .= "<p>This project maintains a p2 repository of binary artifacts. Copy and paste this link into the "Install New Software" dialog to install this project's software.</p>\n";
+	//$html .= "<i>http://download.eclipse.org/rmf/updates</i>\n";
+	//$html .= "<p>Note that the repository link will not necessarily display anything meaningful in your browser.</p>\n";
 
 	$html .= printArtifacts($folder);
 
