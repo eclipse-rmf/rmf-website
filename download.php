042e4d609a5ee39bade54ad96fa0e27d341f7075
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2014-03-07 17:55:54 +0100

    minor change

--- download.php
+++ download.php
@@ -172,13 +172,14 @@
 	
 	$folder = $App->getDownloadBasePath()."/rmf/downloads/drops";
 
-	$html .= "<h4>Software Repository</h4>\n";
+	$html .= "<h3>Software Repository</h3>\n";
 	$html .= "<p>This project maintains a p2 repository of binary artifacts. Copy and paste this link into the \"Install New Software\" dialog to install this project's software.</p>\n";
 	
-	$html .= "<h5>Snapshots (Will be deprecated soon)</h5><p><i>http://download.eclipse.org/rmf/updates/snapshot</i></p>\n";
-	$html .= "<h5>Snapshots (Contains the latest build)</h5><p><i>http://download.eclipse.org/rmf/updates/latest</i></p>\n";
-	$html .= "<h5>Snapshots (Will be published soon)</h5><p><i>http://download.eclipse.org/rmf/updates/releases</i></p>\n";
-	$html .= "<h5>Snapshots (Will be published soon)</h5><p><i>http://download.eclipse.org/rmf/updates/milestones</i></p>\n";
+	$html .= "<h4>Snapshots (Will be deprecated soon)</h4><p><i>http://download.eclipse.org/rmf/updates/snapshot</i></p>\n";
+	$html .= "<h4>Latest Build</h4><p><i>http://download.eclipse.org/rmf/updates/latest</i></p>\n";
+	$html .= "<h4>Milestones (Will be published soon)</h4><p><i>http://download.eclipse.org/rmf/updates/milestones</i></p>\n";
+	
+	$html .= "<h4>Releases (Will be published soon)</h4><p><i>http://download.eclipse.org/rmf/updates/releases</i></p>\n";
 	$html .= "<p>Note that the repository link will not necessarily display anything meaningful in your browser.</p>\n";
 
 	$html .= printArtifacts($folder);
